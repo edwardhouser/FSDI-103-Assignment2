0ecde3b5691cb663948b8b26df317566dca76e01
edwardhouser <edward.houser@gmail.com>
2025-02-03 11:19:10 -0800

    Add files via upload

---
+++ Temperature/scripts/assignment4.js
@@ -0,0 +1,12 @@
+function convertTempRange(){
+    var dropdown = document.getElementById("conversion").value
+    let startValue = Number(prompt("Please enter a starting temperature"))
+    let endValue = Number(prompt("Please enter an ending temperature"))
+    console.log(dropdown);
+    console.log(startValue);
+    console.log(endValue);
+    for(let i==startValue; i<==endValue;i++){
+        console.log(i);
+    }
+
+}
